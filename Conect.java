f85f29a15ed857ec6c7a3ab3af41f32ba52a4784
Kaike silva <107559196+devkaike@users.noreply.github.com>
2022-12-06 21:35:10 -0300

    Update Conect.java

--- Conect.java
+++ Conect.java
@@ -1,30 +1,84 @@
-package conectar;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Conect {
-	private String login = "postgres";
-	private String senha = "aluno";
-	private String host = "5432";
-	private String dbNome = "jogador";
-	private String url = "jdbc:postgres://localhost:"+host+"/"+dbNome;
-	
+
+	private String login;
+	private String senha;
+	private String host;
+	private String dbName;
+	private String url;
+
 	public Connection conexao = null;
-	
-	public Conect() {}
-	
-	public Connection getConnection() {
+
+	public Conect() {
+		super();
+		this.login = "postgres";
+		this.senha = "aluno";
+		this.host = "localhost:5432";
+		this.dbName = "jogador";
+		this.url = "jdbc:postgresql://" + getHost() + "/" + getDbName();
+
 		try {
 			Class.forName("org.postgresql.Driver");
-		}catch(ClassNotFoundException e) {
-			return null;
+			conexao = DriverManager.getConnection(getUrl(), getLogin(), getSenha());
+			System.out.println("Sucesso!");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		try {
-			this.conexao = (Connection) DriverManager.getConnection(url,login,senha);
-		}catch(SQLException ex){
-			return null;
-		}
-		return this.conexao;
 	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getDbName() {
+		return dbName;
+	}
+
+	public void setDbName(String dbName) {
+		this.dbName = dbName;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public int executaSQL(String sql) {
+		try {
+			Statement stm = conexao.createStatement();
+			int res = stm.executeUpdate(sql);
+			conexao.close();
+			return res;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
 }
